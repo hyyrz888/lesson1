033ea1bd1913172728d8bd3e0204c56935e99072
chenhaihu <hyyrz_888@163.com>
2024-09-05 23:55:00 +0800

    :bug: 部署路径

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vuePlugin from '@vitejs/plugin-vue'
 
 export default defineConfig({
-	base: '/deploy_test/',
+	base: '/lesson1/',
 	server: {
 		port: 8888,
 	},
